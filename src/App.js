a20ef883c4f77610f37444e1a5092f6c02a70848
FeZanette <fernandazanette1975@gmail.com>
2023-09-10 19:47:22 -0300

    Aula exercicios filtros, ordenação e busca finalizada

--- src/App.js
+++ src/App.js
@@ -4,23 +4,26 @@ import pokemons from "./pokemon/pokemon.json";
 import PokemonCard from "./components/PokemonCard/PokemonCard";
 import { getColors } from "./utils/ReturnCardColor";
 import Header from "./components/Header/Header.js";
+
 const GlobalStyle = createGlobalStyle`
   *{
     padding: 0;
     margin: 0;
     box-sizing: border-box;
     font-family: "Inter", sans-serif;
-  
-  }
+    }
 `;
 const CardsContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(440px, 1fr));
   justify-items: center;
 `;
+
 function App() {
   const [pesquisa, setPesquisa] = useState("");
   const [idFilter, setIdFilter] = useState("");
+  const [pokeType, setPokeType] = useState("");
+  const [order, setOrder] = useState("");
 
   return (
     <>
@@ -30,13 +33,51 @@ function App() {
         setIdFilter={setIdFilter}
         pesquisa={pesquisa}
         setPesquisa={setPesquisa}
+        pokeType={pokeType}
+        setPokeType={setPokeType}
+        order={order}
+        setOrder={setOrder}
       />
       <CardsContainer>
-        {pokemons.filter((pokemon) => {
-          return idFilter ? pokemon.id.includes(idFilter) : pokemon
-        })
+        {pokemons
           .filter((pokemon) => {
-            return pokemon.name.english.toLowerCase().includes(pesquisa.toLowerCase());
+            return idFilter ? pokemon.id.includes(idFilter) : pokemon;
+          })
+          .filter((pokemon) => {
+            return pokemon.name.english
+              .toLowerCase()
+              .includes(pesquisa.toLowerCase());
+          })
+          .filter((item) => {
+            if (pokeType !== "") {
+              return item.type.includes(pokeType);
+            } else {
+              return item;
+            }
+          })
+          .sort((a, b) => {
+            
+            // USANDO TERNÁRIO PARA FAZER A LÓGICA
+            if (order === "asc") {
+              return a.name.english > b.name.english ? 1 : -1;
+            }
+            if (order === "desc") {
+              return b.name.english > a.name.english ? 1 : -1;
+            }
+
+            // USANDO if/else PARA FAZER A LÓGICA
+            // if (order === "asc") {
+            //   if (a.name.english > b.name.english) {
+            //     return 1;
+            //   } else if (a.name.english < b.name.english) {
+            //     return -1;
+            //   } else {
+            //     return 0;
+            //   }
+            // USANDO localeCompare PARA FAZER A LÓGICA
+            // } else if (order === "desc") {
+            //   return b.name.english.localeCompare(a.name.english);
+            // }
           })
           .map((pokemon) => {
             return (
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 export const Container = styled.header`
     display: flex;
+    flex-wrap: wrap;
     gap: 16px;
     padding: 16px;
     border: 1px solid #000;
